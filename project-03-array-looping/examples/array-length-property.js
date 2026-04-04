2f7d0b4145756636228387d9c8ac9ba6bd354df0
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-04 10:05:48 -0400

    Update array-length-property.js

--- project-03-array-looping/examples/array-length-property.js
+++ project-03-array-looping/examples/array-length-property.js
@@ -1,1 +1,18 @@
+// JavaScript arrays
 
+// Define an array of fruits
+const fruits = ['apple', 'banana', 'cherry', 'mango'];
+
+// Define an image array
+const images = ['mountain.jpg', 'fashion-girl.jpg', 'kitty.jpg'];
+
+// The length of an array
+// How many elements does the array contain?
+
+console.log(`Length of fruits array: ${fruits.length}`)
+// Output would be:
+// Length of fruits array: 4
+
+console.log(`Length of images array: ${images.length}`)
+// Output would be:
+// Length of images array: 3
