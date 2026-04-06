1b7f2f25fd2ea42904ef08925d7dec586dd0c2ec
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-06 07:49:28 -0400

    Update starter.js

--- project-03-array-looping/starter/starter.js
+++ project-03-array-looping/starter/starter.js
@@ -2,7 +2,7 @@
 // Current date
 // Project title
 
-// TODO 1: Use const to create an array of five superheroes
+// TODO 1: Use const to create an array of five continents
 
 // TODO 2: Use a traditional FOR loop to loop through and display -- using console.log() and template strings -- each array element
 
