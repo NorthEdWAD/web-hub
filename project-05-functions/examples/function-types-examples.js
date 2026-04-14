41828375f9e0d54fc8232a0d56879ec5e7625bc4
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-14 07:53:33 -0400

    Update function-types-examples.js

--- project-05-functions/examples/function-types-examples.js
+++ project-05-functions/examples/function-types-examples.js
@@ -1,3 +1,28 @@
+// Traditional function 
+// Using variables to send arguments to function
+function calcProduct(num1, num2) {
+    const myProduct = num1 * num2;
+    return myProduct;
+}
+
+// Declare variables
+const firstNum = 5;
+const secondNum = 3;
+// Call calcProduct ( ) function
+const finalAnswer = calcProduct(firstNum, secondNum);
+console.log(`${firstNum} times ${secondNum} is: ${finalAnswer}`);
+
+// Define function that multiplies two numbers together
+function multiplyTwoNumbers(num3, num4) {
+    const answer = num3 * num4;
+    return answer;
+}
+
+// Call the function and pass two arguments up to the function
+const myAnswer = multiplyTwoNumbers(6, 4);
+const outputString = `6 X 4 = ${myAnswer}`;
+alert(outputString);
+
 
 // Traditional function
 function addTraditional(a, b) {
