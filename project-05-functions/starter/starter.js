f2f5c331c89e0fa7c623cc3a1a201b779135dbe5
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-14 07:42:19 -0400

    Update starter.js

--- project-05-functions/starter/starter.js
+++ project-05-functions/starter/starter.js
@@ -1,12 +1,25 @@
-// Mike Jenkins
-// 05 OCT 20XX
-// Project
+// Your first and last name
+// 14 APR 2026
+// Parameters & Arguments Practice
 
-const greeting = 'Hello! How are you today?';
-console.log(greeting);
+// PART 1
+// TODO: Write a JS function that takes two parameters -- firstName and homeSchool
+// Function should RETURN a template string similar to this example: My name is Mike and my home school is TC Central.
+// Call the function and use two arguments -- one for the firstName, the other for the home school
+// Use the alert ( ) method to display your template string in a pop-up window
 
 
+// PART 2
+// TODO: Write another JS function that takes two parameters -- base and height
+// Function should CALCULATE and RETURN a template string like this: My right triangle has a base of 20 ft., a height of 15 ft., and an area of 150 sq. ft.
+// Call the function and use two arguments -- one for the length of the base, the other for the triangle's height
+// Again, use the alert ( ) method to display your template string in a pop-up window
 
+// PART 3
+// Create a PUBLIC GitHub repo: arguments-parameters-practice
+// Upload your updated starter.js script to the repo
+// Copy the web address of the repo
+// Email repo web address to teacher 
 
 
 
