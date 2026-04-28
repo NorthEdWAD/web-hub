3e732224ff5295b173d9a521798d462e6c65f7d5
bengal865 <69059209+bengal865@users.noreply.github.com>
2026-04-28 11:06:22 -0400

    Add files via upload

---
+++ project-04-if-statements/starter/gatekeeper-lab.js
@@ -0,0 +1,13 @@
+// First and last name
+// Current date
+// JS Gatekeeper Lab
+
+// Write a simple Weather App script
+
+// TODO 1: Declare (create) a variable isRaining and set it to true
+
+// TODO 2: Write an if-else statement that checks the weather conditions a
+// and tells the user what to do
+
+// If it's raining, use console.log() to tell the user to "Bring an umbrella!"
+// Otherwise, use console.log() to tell the user to "Enjoy the sunshine!"
